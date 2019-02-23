c8cd4a816ce356958e5aa166488ed44267e4c135
boyzzy1995 <872778957@qq.com>
2019-02-23 20:19:54 +0800

    add recommand

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="recommand-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommandList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src='item.imgUrl'/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommandList: [{
+        id: '001',
+        title: '故宫',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }, {
+        id: '002',
+        title: '故宫',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        desc: '世界五大宫之首，穿越与您近在咫尺'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommand-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom: 33.9%
+    .item-img
+      width:100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
